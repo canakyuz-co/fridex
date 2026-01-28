b13a245e06ecfec1e6b24118c6dc118d3de893a3
Can Akyüz <canakyuz23@outlook.com>
2026-01-28 03:36:53 +0300

    Monaco dil importlarini dogru katkilarla guncelle

--- src/features/editor/components/EditorView.tsx
+++ src/features/editor/components/EditorView.tsx
@@ -4,12 +4,11 @@ import Close from "lucide-react/dist/esm/icons/x";
 import { useCallback, useEffect, useMemo, useRef } from "react";
 import { EditorPlaceholder } from "./EditorPlaceholder";
 
-import "monaco-editor/esm/vs/basic-languages/css/css.contribution";
-import "monaco-editor/esm/vs/basic-languages/html/html.contribution";
-import "monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution";
-import "monaco-editor/esm/vs/basic-languages/json/json";
+import "monaco-editor/esm/vs/language/css/monaco.contribution";
+import "monaco-editor/esm/vs/language/html/monaco.contribution";
+import "monaco-editor/esm/vs/language/json/monaco.contribution";
+import "monaco-editor/esm/vs/language/typescript/monaco.contribution";
 import "monaco-editor/esm/vs/basic-languages/markdown/markdown.contribution";
-import "monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution";
 
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
